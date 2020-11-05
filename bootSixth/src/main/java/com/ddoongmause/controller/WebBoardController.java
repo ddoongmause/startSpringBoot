faf8cfc17fe82163746db3c0b91aaf3ba8f8dacb
yhs <yhs@onlinepowers.com>
2020-11-05 17:20:19 +0900

    P.281하던거 하....왜 DB안열려ㅠ

--- bootSixth/src/main/java/com/ddoongmause/controller/WebBoardController.java
+++ bootSixth/src/main/java/com/ddoongmause/controller/WebBoardController.java
@@ -1,11 +1,16 @@
 package com.ddoongmause.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ddoongmause.domain.WebBoard;
+import com.ddoongmause.persistence.WebBoardRepository;
+import com.ddoongmause.vo.PageVO;
 
 import lombok.extern.java.Log;
 
@@ -14,8 +19,30 @@ import lombok.extern.java.Log;
 @Log
 public class WebBoardController {
 	
+	@Autowired
+	private WebBoardRepository repo;
+	
+	/*
 	@GetMapping("/list")
-	public void list(@PageableDefault(direction=Sort.Direction.DESC, sort="bno", size=10, page=0) Pageable page) {
-		log.info("list() called..." + page);
+	public void list(PageVO vo) {
+		
+		Pageable page = vo.makePageable(0, "bno");
+		
+		log.info("" + page);
 	}
+	*/
+	
+	@GetMapping("/list")
+	public void list(PageVO vo, Model model) {
+		
+		Pageable page = vo.makePageable(0, "bno");
+		
+		Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), page);
+		
+		log.info("" + page);
+		log.info(""+result);
+		
+		model.addAttribute("result", result);
+	}
+	
 }
--- bootSixth/src/main/java/com/ddoongmause/vo/PageVO.java
+++ bootSixth/src/main/java/com/ddoongmause/vo/PageVO.java
@@ -1,5 +1,9 @@
 package com.ddoongmause.vo;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 public class PageVO {
 	
 	private static final int DEFAULT_SIZE = 10;
@@ -17,5 +21,21 @@ public class PageVO {
 		return page;
 	}
 	
+	public void setPage(int page) {
+		this.page = page < 0 ? 1: page;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size < DEFAULT_SIZE || size > DEFAULT_MAX_SIZE ? DEFAULT_SIZE: size;
+	}
+	
+	public Pageable makePageable(int direction, String... props) {
+		Sort.Direction dir = direction == 0 ? Sort.Direction.DESC : Sort.Direction.ASC;
+		return PageRequest.of(this.page -1, this.size, dir, props);
+	}
 
 }
