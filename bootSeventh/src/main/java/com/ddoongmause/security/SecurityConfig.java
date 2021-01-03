a9e05b3b012d6981c6c310df9575ea6e28020e26
DDoongMa <DDoongMause@gmail.com>
2021-01-03 21:58:23 +0900

    P.422 밑에 코드 친상태고 423 엑세스 디나이부터 하면 됨

--- bootSeventh/src/main/java/com/ddoongmause/security/SecurityConfig.java
+++ bootSeventh/src/main/java/com/ddoongmause/security/SecurityConfig.java
@@ -1,5 +1,36 @@
 package com.ddoongmause.security;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import lombok.extern.java.Log;
+
+@Log
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		//super.configure(http);
+		log.info("security config.............");
+		
+		http.authorizeRequests()
+			.antMatchers("/guest/**").permitAll()
+			.antMatchers("/manager/**").hasRole("MANAGER")
+			.antMatchers("/admin/**").hasRole("ADMIN");
+		
+		http.formLogin().loginPage("/login");
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)throws Exception{
+		log.info("build Auth global...........");
+		
+		auth.inMemoryAuthentication().withUser("manager").password("{noop}1111").roles("MANAGER");
+	}
+
+	
 }
---
+++ bootSeventh/src/test/java/com/ddoongmause/MemberTests.java
@@ -0,0 +1,56 @@
+package com.ddoongmause;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ddoongmause.domain.Member;
+import com.ddoongmause.domain.MemberRole;
+import com.ddoongmause.persistence.MemberRepository;
+
+import lombok.extern.java.Log;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Log
+@Commit
+public class MemberTests {
+
+	@Autowired
+	private MemberRepository repo;
+	
+	@Test
+	public void testInsert() {
+		for(int i=0; i<100; i++) {
+			Member member = new Member();
+			member.setUid("user" + i);
+			member.setUpw("pw" + i);
+			member.setUname("사용자" + i);
+			
+			MemberRole role = new MemberRole();
+			if(i <= 80) {
+				role.setRoleName("BASIC");
+			}else if(i <= 90) {
+				role.setRoleName("MANAGER");
+			}else {
+				role.setRoleName("ADMIN");
+			}
+			member.setRoles(Arrays.asList(role));
+			 
+			repo.save(member);
+		}
+	}
+	
+	@Test
+	public void testRead() {
+		Optional<Member> result = repo.findById("user85");
+		result.ifPresent(member -> log.info("member" + member));
+		
+	}
+}
